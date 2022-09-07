70f427e6486f79079f7b6e3757150a0f47f7ec17
Richard W. L. Brehmer <wlb.richard@gmail.com>
2022-09-07 19:55:02 -0300

    feature: adjust path of storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
-import theme from '../styles/theme';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
