8976f8ec4ca26ad935a74aaa04fe83bec28d1b0d
Richard Brehmer <richard.brehmer@everest-erp.com>
2023-02-26 20:27:03 -0300

    feat: add Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,18 @@
 import * as S from './styles';
 
-const Button = () => (
-  <S.wrapper>
-    <h3>Button</h3>
+export type ButtonProps = {
+  children?: React.ReactNode;
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+};
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false
+}: ButtonProps) => (
+  <S.wrapper fullWidth={fullWidth} size={size}>
+    {!!children && <span>{children}</span>}
   </S.wrapper>
 );
 
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,15 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import Button from '.';
 
 describe('Button', () => {
-  it('Should render the component Button', () => {
-    const { container } = render(<Button />);
+  it('Should render the medium size button by default', () => {
+    renderWithTheme(<Button>Subscribe</Button>);
+    expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
+      height: '4rem',
+      'font-size': '1.4rem',
+      padding: '0.8rem 3.2rem'
+    });
+  });
 
-    expect(
-      screen.getByRole('heading', { name: /Button/i })
-    ).toBeInTheDocument();
+  it('Should render the small size button', () => {
+    renderWithTheme(<Button size="small">Subscribe</Button>);
+    expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    });
+  });
 
-    expect(container.firstChild).toMatchSnapshot();
+  it('Should render the large size button', () => {
+    renderWithTheme(<Button size="large">Subscribe</Button>);
+    expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem'
+    });
+  });
+
+  it('Should render the button with full width', () => {
+    renderWithTheme(<Button fullWidth>Subscribe</Button>);
+    expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
+      width: '100%'
+    });
   });
 });
