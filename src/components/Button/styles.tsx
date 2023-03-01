fa0d9bc695812032302a19bb97954ec2ed8c78d5
Richard Brehmer <richard.brehmer@everest-erp.com>
2023-02-28 22:18:47 -0300

    feat: add icon on Button

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,7 +1,10 @@
 import styled, { css, DefaultTheme } from 'styled-components';
 import { ButtonProps } from '.';
 
-type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>;
+type WrapperProps = { hasIcon: boolean } & Pick<
+  ButtonProps,
+  'size' | 'fullWidth'
+>;
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -20,17 +23,30 @@ const wrapperModifiers = {
   `,
   fullWidth: () => css`
     width: 100%;
+  `,
+  withIcon: (theme: DefaultTheme) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      width: 1.5rem;
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
   `
 };
 
 export const wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth }) => css`
+  ${({ theme, size, fullWidth, hasIcon }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    cursor: pointer;
 
+    ${hasIcon && wrapperModifiers.withIcon(theme)}
     ${!!size && wrapperModifiers[size](theme)}
     ${!!fullWidth && wrapperModifiers.fullWidth()}
   `}
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,16 +1,19 @@
 import { screen } from '@testing-library/react';
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import Button from '.';
 
 describe('Button', () => {
   it('Should render the medium size button by default', () => {
-    renderWithTheme(<Button>Subscribe</Button>);
+    const { container } = renderWithTheme(<Button>Subscribe</Button>);
     expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
       height: '4rem',
       'font-size': '1.4rem',
       padding: '0.8rem 3.2rem'
     });
+
+    expect(container).toMatchSnapshot();
   });
 
   it('Should render the small size button', () => {
@@ -35,5 +38,14 @@ describe('Button', () => {
     expect(screen.getByRole('button', { name: /Subscribe/i })).toHaveStyle({
       width: '100%'
     });
+  });
+
+  it('Should render the button with icon', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Subscribe</Button>
+    );
+
+    expect(screen.getByText(/subscribe/i)).toBeInTheDocument();
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
   });
 });
